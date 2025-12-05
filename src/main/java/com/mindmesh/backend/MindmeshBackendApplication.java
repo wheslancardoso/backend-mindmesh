5e6592643dd5c8f1b86d14464c20e02d644b2977
wheslancardoso <wheslancardoso1@gmail.com>
2025-12-05 17:52:16 -0300

    feat: Commit inicial da estrutura do projeto Spring Boot

---
+++ src/main/java/com/mindmesh/backend/MindmeshBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mindmesh.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindmeshBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindmeshBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindmesh/backend/MindmeshBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindmesh.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindmeshBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
